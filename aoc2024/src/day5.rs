3a6580df98252059081a392324d4c87b05c61213
li-daqian <lidaqian11@gmail.com>
2025-03-11 17:17:40 +0800

    feat: day5 part2

--- aoc2024/src/day5.rs
+++ aoc2024/src/day5.rs
@@ -44,6 +44,80 @@ pub fn part1(input: &str) -> usize {
     answer
 }
 
+fn topo_sort(
+    a: usize,
+    order: &mut Vec<usize>,
+    visited: &mut HashMap<usize, usize>,
+    present: &HashSet<usize>,
+    edges: &HashMap<usize, Vec<usize>>,
+) {
+    order.push(a);
+    for b in edges.get(&a).unwrap_or(&Vec::new()) {
+        if present.contains(b) {
+            if *visited.entry(*b).and_modify(|e| *e -= 1).or_insert(0) == 0 {
+                topo_sort(*b, order, visited, present, edges);
+            }
+        }
+    }
+}
+
+#[aoc(day5, part2)]
+pub fn part2(input: &str) -> usize {
+    let mut answer = 0usize;
+    let mut edges: HashMap<usize, Vec<usize>> = HashMap::new();
+    input.lines().for_each(|line| {
+        if line.is_empty() {
+            return;
+        }
+        if let Some((l, r)) = line.split_once("|") {
+            if let (Ok(l), Ok(r)) = (l.parse::<usize>(), r.parse::<usize>()) {
+                edges.entry(l).or_insert_with(Vec::new).push(r);
+            }
+        } else {
+            let page_numbers: Vec<usize> = line
+                .split(",")
+                .map(|num| num.parse::<usize>().unwrap())
+                .collect();
+
+            let mut earlier: HashSet<usize> = HashSet::new();
+            let mut ok = true;
+            let mut visited: HashMap<usize, usize> = HashMap::new();
+            let present: HashSet<usize> = page_numbers.iter().copied().collect();
+            for page_number in &page_numbers {
+                edges
+                    .get(&page_number)
+                    .unwrap_or(&Vec::new())
+                    .iter()
+                    .for_each(|after| {
+                        if present.contains(after) {
+                            *visited.entry(*after).or_insert(0) += 1;
+                        }
+                        if earlier.contains(after) {
+                            ok = false;
+                        }
+                    });
+                earlier.insert(*page_number);
+            }
+
+            if !ok {
+                let mut order: Vec<usize> = Vec::new();
+                let mut starting: Vec<usize> = Vec::new();
+                for page_number in &page_numbers {
+                    if visited.get(page_number).unwrap_or(&0) == &0 {
+                        starting.push(*page_number);
+                    }
+                }
+                for x in starting {
+                    topo_sort(x, &mut order, &mut visited, &present, &edges);
+                }
+                answer += order.get(order.len() / 2).unwrap_or(&0);
+            }
+        }
+    });
+
+    answer
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -83,5 +157,10 @@ mod tests {
     #[test]
     pub fn part1_example() {
         assert_eq!(part1(SAMPLE), 143);
+    }
+
+    #[test]
+    pub fn part2_example() {
+        assert_eq!(part2(SAMPLE), 123);
     }
 }

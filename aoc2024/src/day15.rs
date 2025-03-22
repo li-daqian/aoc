47d0cd198edc0d0b6a5a41ca3072570eaabf16e8
li-daqian <lidaqian11@gmail.com>
2025-03-22 21:37:16 +0800

    feat: day15

--- aoc2024/src/day15.rs
+++ aoc2024/src/day15.rs
@@ -1,24 +1,237 @@
 use std::fmt::Display;
+use std::mem;
+use std::ops::Add;
 
 use aoc_runner_derive::aoc;
-
-#[derive(Default)]
-struct WareHouse {
-    map: Vec<Vec<char>>,
-    height: usize,
-    width: usize,
-    pos: (usize, usize),
+#[derive(PartialEq, Clone, Copy, Debug)]
+enum Tile {
+    Empty,
+    Wall,
+    Object,
+    Robot,
+    BoxLeft,
+    BoxRight,
 }
 
-impl Display for WareHouse {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for (i, row) in self.map.iter().enumerate() {
-            for (j, cell) in row.iter().enumerate() {
-                if (i, j) == self.pos {
-                    write!(f, "@")?;
-                } else {
-                    write!(f, "{}", cell)?;
+impl From<char> for Tile {
+    fn from(value: char) -> Self {
+        match value {
+            '#' => Self::Wall,
+            'O' => Self::Object,
+            '@' => Self::Robot,
+            '[' => Self::BoxLeft,
+            ']' => Self::BoxRight,
+            _ => Self::Empty,
+        }
+    }
+}
+
+impl Tile {
+    fn double(self) -> impl Iterator<Item = Self> {
+        match self {
+            Tile::Empty => [Tile::Empty, Tile::Empty].into_iter(),
+            Tile::Wall => [Tile::Wall, Tile::Wall].into_iter(),
+            Tile::Object => [Tile::BoxLeft, Tile::BoxRight].into_iter(),
+            Tile::Robot => [Tile::Robot, Tile::Empty].into_iter(),
+            Tile::BoxLeft | Tile::BoxRight => panic!(),
+        }
+    }
+}
+
+impl From<Tile> for char {
+    fn from(val: Tile) -> Self {
+        match val {
+            Tile::Empty => '.',
+            Tile::Wall => '#',
+            Tile::Object => 'O',
+            Tile::Robot => '@',
+            Tile::BoxLeft => '[',
+            Tile::BoxRight => ']',
+        }
+    }
+}
+
+struct Warehouse {
+    grid: Vec<Vec<Tile>>,
+    robot: (usize, usize),
+    width: usize,
+    height: usize,
+}
+
+impl Warehouse {
+    fn new(input: &str) -> Self {
+        let mut grid: Vec<Vec<Tile>> = Vec::new();
+        let mut robot = (0, 0);
+
+        for (row, line) in input.lines().enumerate() {
+            let mut grid_row = Vec::new();
+
+            for (col, char) in line.char_indices() {
+                let tile = Tile::from(char);
+
+                if tile == Tile::Robot {
+                    robot = (row, col);
                 }
+
+                grid_row.push(tile);
+            }
+
+            grid.push(grid_row);
+        }
+
+        let height = grid.len();
+        let width = grid[0].len();
+
+        Self {
+            grid,
+            robot,
+            width,
+            height,
+        }
+    }
+
+    fn scale_width(&mut self) {
+        let mut temp: Vec<Vec<Tile>> = Vec::new();
+        mem::swap(&mut self.grid, &mut temp);
+
+        self.grid = temp
+            .into_iter()
+            .map(|row| row.into_iter().flat_map(Tile::double).collect())
+            .collect();
+
+        self.width *= 2;
+        self.robot.1 *= 2;
+    }
+
+    fn move_robot(&mut self, direction: Direction) {
+        let (row, col) = self.robot;
+
+        if self.can_move_tile(row, col, direction) {
+            self.move_tile(row, col, direction);
+            self.robot = self.robot + direction;
+        }
+    }
+
+    fn move_tile(&mut self, row: usize, col: usize, direction: Direction) {
+        let (next_row, next_col) = (row, col) + direction;
+        let next_tile = self.grid[next_row][next_col];
+
+        match next_tile {
+            Tile::Empty => {
+                self.grid[next_row][next_col] = self.grid[row][col];
+                self.grid[row][col] = Tile::Empty;
+            }
+            Tile::Object => {
+                self.move_tile(next_row, next_col, direction);
+                self.grid[next_row][next_col] = self.grid[row][col];
+                self.grid[row][col] = Tile::Empty;
+            }
+            Tile::BoxRight => {
+                self.move_tile(next_row, next_col - 1, direction);
+                self.move_tile(next_row, next_col, direction);
+                self.grid[next_row][next_col] = self.grid[row][col];
+                self.grid[row][col] = Tile::Empty;
+            }
+            Tile::BoxLeft => {
+                self.move_tile(next_row, next_col + 1, direction);
+                self.move_tile(next_row, next_col, direction);
+                self.grid[next_row][next_col] = self.grid[row][col];
+                self.grid[row][col] = Tile::Empty;
+            }
+            Tile::Wall => panic!(),
+            Tile::Robot => panic!(),
+        }
+    }
+
+    fn can_move_tile(&self, row: usize, col: usize, direction: Direction) -> bool {
+        let (next_row, next_col) = (row, col) + direction;
+        let next_tile = self.grid[next_row][next_col];
+
+        match next_tile {
+            Tile::Empty => true,
+            Tile::Wall => false,
+            Tile::Object => self.can_move_tile(next_row, next_col, direction),
+            Tile::BoxLeft => {
+                if direction == Direction::Left {
+                    self.can_move_tile(next_row, next_col, direction)
+                } else if direction == Direction::Right {
+                    self.can_move_tile(next_row, next_col + 1, direction)
+                } else {
+                    self.can_move_tile(next_row, next_col + 1, direction)
+                        && self.can_move_tile(next_row, next_col, direction)
+                }
+            }
+            Tile::BoxRight => {
+                if direction == Direction::Right {
+                    self.can_move_tile(next_row, next_col, direction)
+                } else if direction == Direction::Left {
+                    self.can_move_tile(next_row, next_col - 1, direction)
+                } else {
+                    self.can_move_tile(next_row, next_col - 1, direction)
+                        && self.can_move_tile(next_row, next_col, direction)
+                }
+            }
+            Tile::Robot => panic!(),
+        }
+    }
+
+    fn gps_coordinate(row: usize, col: usize) -> usize {
+        row * 100 + col
+    }
+
+    fn sum_gps_coordinates(&self) -> usize {
+        let mut sum = 0;
+        for row in 0..self.height {
+            for col in 0..self.width {
+                if self.grid[row][col] == Tile::Object || self.grid[row][col] == Tile::BoxLeft {
+                    sum += Self::gps_coordinate(row, col);
+                }
+            }
+        }
+
+        sum
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Add<Direction> for (usize, usize) {
+    type Output = (usize, usize);
+
+    fn add(self, rhs: Direction) -> Self::Output {
+        let (row, col) = self;
+        match rhs {
+            Direction::Up => (row - 1, col),
+            Direction::Down => (row + 1, col),
+            Direction::Left => (row, col - 1),
+            Direction::Right => (row, col + 1),
+        }
+    }
+}
+
+impl From<char> for Direction {
+    fn from(value: char) -> Self {
+        match value {
+            '^' => Self::Up,
+            '>' => Self::Right,
+            '<' => Self::Left,
+            'v' => Self::Down,
+            _ => panic!(),
+        }
+    }
+}
+
+impl Display for Warehouse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for row in &self.grid {
+            for tile in row {
+                write!(f, "{}", char::from(*tile))?;
             }
             writeln!(f)?;
         }
@@ -26,264 +239,42 @@ impl Display for WareHouse {
     }
 }
 
-impl WareHouse {
-    #[inline]
-    fn set(&mut self, (x, y): (usize, usize), c: char) {
-        self.map[x][y] = c;
-    }
-
-    #[inline]
-    fn next_pos(&self, pos: (usize, usize), dir: (isize, isize)) -> Option<(usize, usize)> {
-        let (x, y) = (pos.0 as isize + dir.0, pos.1 as isize + dir.1);
-        if x < 0 || x >= self.height as isize || y < 0 || y >= self.width as isize {
-            return None;
-        }
-        Some((x as usize, y as usize))
-    }
-
-    #[inline]
-    fn get(&self, (x, y): (usize, usize)) -> char {
-        self.map[x][y]
-    }
-
-    fn find_empty_pos(&self, pos: (usize, usize), dir: (isize, isize)) -> Option<(usize, usize)> {
-        let next_p = self.next_pos(pos, dir)?;
-        let next = self.get(next_p);
-        if next == '#' {
-            return None;
-        } else if next == '.' {
-            return Some(next_p);
-        } else {
-            return self.find_empty_pos(next_p, dir);
-        }
-    }
-
-    fn try_move(&mut self, dir: char) {
-        let dir = make_dir(dir);
-        if let Some(next_pos) = self.next_pos(self.pos, dir) {
-            let next = self.get(next_pos);
-            if next == '#' {
-                return;
-            }
-            if next == '.' {
-                self.pos = next_pos;
-            } else {
-                if let Some(empty_pos) = self.find_empty_pos(next_pos, dir) {
-                    self.set(next_pos, '.');
-                    self.set(empty_pos, 'O');
-                    self.pos = next_pos;
-                }
-            }
-        }
-    }
-
-    fn try_move_2(&mut self, dir: char) {
-        let dir = make_dir(dir);
-        if let Some(next_pos) = self.next_pos(self.pos, dir) {
-            let next = self.get(next_pos);
-            if next == '#' {
-                return;
-            }
-            if next == '.' {
-                self.pos = next_pos;
-            } else {
-                let mut foods: Vec<(usize, usize, char)> = vec![];
-                if self.move_food(&mut foods, next_pos, dir) {
-                    foods.reverse();
-                    foods.iter().for_each(|(x, y, c)| {
-                        self.set(self.next_pos((*x, *y), dir).unwrap(), *c);
-                        self.set((*x, *y), '.');
-                    });
-                    self.set(next_pos, '.');
-                    self.pos = next_pos;
-                }
-            }
-        }
-    }
-
-    fn move_food(
-        &self,
-        foods: &mut Vec<(usize, usize, char)>,
-        pos: (usize, usize),
-        dir: (isize, isize),
-    ) -> bool {
-        if dir.1 == 0 {
-            let current_c1 = self.get(pos);
-            let pos_2 = if current_c1 == '[' {
-                self.next_pos(pos, (0, 1))
-            } else if current_c1 == ']' {
-                self.next_pos(pos, (0, -1))
-            } else {
-                None
-            };
-            if pos_2.is_none() {
-                unreachable!();
-            }
-            let next_pos_1 = self.next_pos(pos, dir);
-            if next_pos_1.is_none() {
-                return false;
-            }
-            let next_pos_2 = self.next_pos(pos_2.unwrap(), dir);
-            if next_pos_2.is_none() {
-                return false;
-            }
-            let (next_c1, next_c2) = (self.get(next_pos_1.unwrap()), self.get(next_pos_2.unwrap()));
-            if next_c1 == '#' || next_c2 == '#' {
-                return false;
-            }
-
-            let current_c2 = self.get(pos_2.unwrap());
-            foods.push((pos.0, pos.1, current_c1));
-            foods.push((pos_2.unwrap().0, pos_2.unwrap().1, current_c2));
-            if next_c1 == '.' && next_c2 == '.' {
-                return true;
-            }
-
-            if (next_c1 == '[' || next_c1 == ']') && (next_c2 == '[' || next_c2 == ']') {
-                return self.move_food(foods, next_pos_1.unwrap(), dir)
-                    && self.move_food(foods, next_pos_2.unwrap(), dir);
-            }
-
-            if next_c1 == '[' || next_c1 == ']' {
-                return self.move_food(foods, next_pos_1.unwrap(), dir);
-            }
-            if next_c2 == '[' || next_c2 == ']' {
-                return self.move_food(foods, next_pos_2.unwrap(), dir);
-            }
-            unreachable!()
-        } else {
-            let next_pos = self.next_pos(pos, dir);
-            if next_pos.is_none() {
-                return false;
-            }
-            let next_c = self.get(next_pos.unwrap());
-            if next_c == '#' {
-                return false;
-            }
-
-            foods.push((pos.0, pos.1, self.get(pos)));
-            if next_c == '.' {
-                return true;
-            }
-
-            if next_c == '[' || next_c == ']' {
-                return self.move_food(foods, next_pos.unwrap(), dir);
-            }
-            unreachable!()
-        }
-    }
-}
-
-#[inline]
-fn make_dir(dir: char) -> (isize, isize) {
-    match dir {
-        '^' => (-1, 0),
-        '>' => (0, 1),
-        'v' => (1, 0),
-        '<' => (0, -1),
-        _ => unreachable!(),
-    }
-}
-
 #[aoc(day15, part1)]
 pub fn part1(input: &str) -> usize {
-    let mut ware_house = WareHouse::default();
+    let (warehouse, moves) = input.split_once("\n\n").unwrap();
 
-    let mut is_house = true;
-    input.lines().for_each(|line| {
-        if line.is_empty() {
-            is_house = false;
-            ware_house.height = ware_house.map.len();
-            ware_house.width = ware_house.map[0].len();
-            return;
-        }
-        if is_house {
-            ware_house.map.push(line.chars().collect());
-            if ware_house.pos == (0, 0) {
-                line.find('@').map(|i| {
-                    ware_house.pos = (ware_house.map.len() - 1, i);
-                    ware_house.set(ware_house.pos, '.');
-                });
-            }
-        } else {
-            line.chars().for_each(|c| {
-                ware_house.try_move(c);
-            });
-        }
-    });
+    let mut warehouse = Warehouse::new(warehouse);
+    let moves: Vec<Direction> = moves
+        .lines()
+        .flat_map(|line| line.chars())
+        .map(Direction::from)
+        .collect();
 
-    ware_house
-        .map
-        .iter()
-        .enumerate()
-        .flat_map(|(i, row)| {
-            row.iter().enumerate().filter_map(
-                move |(j, &cell)| {
-                    if cell == 'O' {
-                        Some(i * 100 + j)
-                    } else {
-                        None
-                    }
-                },
-            )
-        })
-        .sum()
+    for direction in moves {
+        warehouse.move_robot(direction);
+    }
+
+    warehouse.sum_gps_coordinates()
 }
 
 #[aoc(day15, part2)]
 pub fn part2(input: &str) -> usize {
-    let mut ware_house = WareHouse::default();
+    let (warehouse, moves) = input.split_once("\n\n").unwrap();
 
-    let mut is_house = true;
-    input.lines().for_each(|line| {
-        if line.is_empty() {
-            is_house = false;
-            ware_house.height = ware_house.map.len();
-            ware_house.width = ware_house.map[0].len();
-            return;
-        }
-        if is_house {
-            let row = line.chars().fold(vec![' '; 0], |mut acc, c| {
-                if c == '#' {
-                    acc.push('#');
-                    acc.push('#');
-                } else if c == '.' {
-                    acc.push('.');
-                    acc.push('.');
-                } else if c == '@' {
-                    ware_house.pos = (ware_house.map.len(), acc.len());
-                    acc.push('.');
-                    acc.push('.');
-                } else if c == 'O' {
-                    acc.push('[');
-                    acc.push(']');
-                }
-                acc
-            });
-            ware_house.map.push(row);
-        } else {
-            line.chars().for_each(|c| {
-                ware_house.try_move_2(c);
-            });
-        }
-    });
+    let mut warehouse = Warehouse::new(warehouse);
+    let moves: Vec<Direction> = moves
+        .lines()
+        .flat_map(|line| line.chars())
+        .map(Direction::from)
+        .collect();
 
-    ware_house
-        .map
-        .iter()
-        .enumerate()
-        .flat_map(|(i, row)| {
-            row.iter().enumerate().filter_map(
-                move |(j, &cell)| {
-                    if cell == '[' {
-                        Some(i * 100 + j)
-                    } else {
-                        None
-                    }
-                },
-            )
-        })
-        .sum()
+    warehouse.scale_width();
+
+    for direction in moves {
+        warehouse.move_robot(direction);
+    }
+
+    warehouse.sum_gps_coordinates()
 }
 
 #[cfg(test)]

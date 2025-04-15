963e4a83dc71023a1e077a01b6ab3348869ce14e
li-daqian <lidaqian11@gmail.com>
2025-04-16 07:57:04 +0800

    feat: 2023 day4

--- aoc2023/src/day4.rs
+++ aoc2023/src/day4.rs
@@ -5,12 +5,13 @@ pub fn part1(input: &str) -> usize {
     input
         .lines()
         .map(|line| {
-            let (_, numbers) = line.split_once(':').unwrap();
-            let (winning_numbers, numbers) = numbers.split_once('|').unwrap();
+            let (_, numbers) = line.split_once(":").unwrap();
+            let (winning_numbers, numbers) = numbers.split_once("|").unwrap();
             let winning_numbers = winning_numbers
                 .split(' ')
                 .map(|n| n.trim().parse::<usize>().unwrap())
                 .collect::<Vec<_>>();
+            numbers.split(" ")
         })
         .sum()
 }

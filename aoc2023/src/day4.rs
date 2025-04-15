27c3fd2dcdedce9cc06a58cee096370fb8aae2a7
li-daqian <lidaqian11@gmail.com>
2025-04-16 07:56:34 +0800

    feat: 2023 day4

---
+++ aoc2023/src/day4.rs
@@ -0,0 +1,16 @@
+use aoc_runner_derive::aoc;
+
+#[aoc(day4, part1)]
+pub fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let (_, numbers) = line.split_once(':').unwrap();
+            let (winning_numbers, numbers) = numbers.split_once('|').unwrap();
+            let winning_numbers = winning_numbers
+                .split(' ')
+                .map(|n| n.trim().parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+        })
+        .sum()
+}
